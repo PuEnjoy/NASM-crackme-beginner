f0715574d282ec1e45223a621da49c12fbba7760
Enjoy <tobias.kregelin@t-online.de>
2024-09-22 17:44:20 +0200

    add Easy - sourcecode.asm

---
+++ Easy/sourcecode.asm
@@ -0,0 +1,50 @@
+section .data
+inputmsg: db "Enter the code: ", 10
+inputmsglen: equ $ - inputmsg
+secret: db "Ronja"
+wrongmsg: db "WORNG!", 10
+correctmsg: db "CORRECT!", 10
+
+section .bss
+inbuffer: resw 1
+
+section .text
+global _start
+
+_start:
+mov rax, 1
+mov rdi, 1
+lea rsi, inputmsg
+mov rdx, inputmsglen
+syscall
+mov rax, 0
+mov rdi, 1
+lea rsi, inbuffer
+mov rdx, 16
+syscall
+
+_checkpw:
+lea rdi, [secret]
+lea rsi, [inbuffer]
+mov rcx, 5
+repe cmpsb
+jne _wrong
+mov rax, 1
+mov rdi, 1
+lea rsi, correctmsg
+mov rdx, 9
+syscall
+jmp _exit
+
+_wrong:
+mov rax, 1
+mov rdi, 1
+lea rsi, wrongmsg
+mov rdx, 7
+syscall
+jmp _exit
+
+_exit:
+mov rax, 60
+mov rdi, 0
+syscall
